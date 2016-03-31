b25acde0a3689b5a5aa6640517b3e23269490d9d
Massimiliano Giustizieri <giustizieri25@hotmail.com>
2016-03-31 23:35:26 +0200

    Converted to console application.

--- WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/Program.cs
@@ -1,22 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Xml;
 
-namespace WindowsFormsApplication1
+namespace SqlParser
 {
     static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            XmlDocument document = new XmlDocument();
+            XmlElement rootElement = document.CreateElement("Statements");
+            document.AppendChild(rootElement);
+
+            string[] lines = File.ReadAllLines("Statements.txt");
+
+            foreach (string line in lines)
+            {
+                XmlDocument xd = new StatementParser(line).Parse();
+                XmlNode newChild = document.ImportNode(xd.FirstChild, true);
+                rootElement.AppendChild(newChild);
+            }
+            document.Save("statements.xml");
         }
     }
 }
--- WindowsFormsApplication1/Tokens.cs
+++ WindowsFormsApplication1/Tokens.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Xml;
 
-namespace WindowsFormsApplication1
+namespace SqlParser
 {
     class StatementElement : XmlElement
     {
